bed9d8537164d3b6bcf21846e41d872421f681af
Vishal <vishalshaw1998@gmail.com>
2020-07-03 20:13:38 +0530

    Some Changes

--- script.js
+++ script.js
@@ -16,29 +16,32 @@ var months = [
 //Getting data from the server
 var content = document.getElementsByClassName("content")[0];
 async function getDetailBySection(section) {
-    while (content.childNodes.length) {
-        content.removeChild(content.childNodes[0]);
-    }
-    var key = "EF2dFbix6XHNAhMhfaUFZGPB3j2NH3CM";
-    if (section == undefined) {
-        var url =
-            "https://api.nytimes.com/svc/topstories/v2//home.json?api-key=EF2dFbix6XHNAhMhfaUFZGPB3j2NH3CM";
-    } else {
-        var url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`;
-    }
-    var response = await fetch(url);
-    var data = await response.json();
-    data.results.forEach((item) => {
-        var capitalSection =
-            item.section[0].toUpperCase() + item.section.slice(1);
-        var month = months[item.created_date.split("-")[1].slice(1) - 1];
-        var day = item.created_date.split("-")[2].slice(0, 2);
-        var tempDiv = document.createElement("div");
-        tempDiv.setAttribute("class", "margin-top");
-        tempDiv.innerHTML = `<div class="card" style="width: 100%;">
+    try {
+        while (content.childNodes.length) {
+            content.removeChild(content.childNodes[0]);
+        }
+        var key = "EF2dFbix6XHNAhMhfaUFZGPB3j2NH3CM";
+        if (section == undefined || section == "Home") {
+            var url =
+                "https://api.nytimes.com/svc/topstories/v2//home.json?api-key=EF2dFbix6XHNAhMhfaUFZGPB3j2NH3CM";
+        } else {
+            var url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`;
+        }
+        var response = await fetch(url);
+        var data = await response.json();
+        data.results.forEach((item) => {
+            var capitalSection =
+                item.section[0].toUpperCase() + item.section.slice(1);
+            if (capitalSection == "Us") {
+                capitalSection = "US";
+            }
+            var month = months[item.created_date.split("-")[1].slice(1) - 1];
+            var day = item.created_date.split("-")[2].slice(0, 2);
+            var tempDiv = document.createElement("div");
+            tempDiv.setAttribute("class", "margin-top");
+            tempDiv.innerHTML = `<div class="card" style="width: 100%;">
         <div class="container">
             <div class="row">
-                <div class="card-body"></div>
                 <div class="col-8 padding">
                     <h6>${capitalSection}</h6>
                     <h5 class="card-title">${item.title}</h5>
@@ -56,7 +59,7 @@ async function getDetailBySection(section) {
                 </div>
                 <div class="col-4 col-img">
                     <img
-                        class="card-img"
+                        class="card-img img-fluid img-responsive"
                         src="${item.multimedia[item.multimedia.length - 1].url}"
                         alt="Card image cap"
                     />
@@ -64,8 +67,11 @@ async function getDetailBySection(section) {
             </div>
         </div>
     </div>`;
-        content.appendChild(tempDiv);
-    });
+            content.appendChild(tempDiv);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 var sections = document.querySelectorAll(".topic");
