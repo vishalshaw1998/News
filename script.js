ee82cc81b90ebe9f9c671470334f7d2795025615
Vishal <vishalshaw1998@gmail.com>
2020-07-03 20:34:24 +0530

    Done

--- script.js
+++ script.js
@@ -12,7 +12,99 @@ var months = [
     "Nov",
     "Dec",
 ];
-
+var container = document.createElement("div");
+container.setAttribute("class", "container");
+container.innerHTML = `<div class="row flex">
+<div class="col align-left flex">
+    <div
+        class="btn btn-link"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapseOne"
+        aria-expanded="true"
+        aria-controls="collapseOne"
+    >
+        <i class="fa fa-bars fa-3x" aria-hidden="true"></i>
+    </div>
+    <div
+        class="heading"
+        style="align-self: center; margin-left: 30%;"
+    >
+        The Good NEWS
+    </div>
+</div>
+</div>
+<div id="collapseOne" class="collapse show forButton">
+<div class="row">
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Arts</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Automobiles</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Books</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Business</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Fashion</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Food</span>
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-md-2 col-sm-6 offset-1">
+        <span class="border-botm topic">Health</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Home</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Insider</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Magazine</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Movies</span>
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">World</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Science</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Sports</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Technology</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Politics</span>
+    </div>
+    <div class="col-md-2 col-sm-6">
+        <span class="border-botm topic">Opinion</span>
+    </div>
+</div>
+<br />
+</div>
+<br />
+<div class="row">
+<div class="col">
+    <div class="content1">
+        <div class="content"></div>
+    </div>
+</div>
+</div>`;
+document.body.appendChild(container);
 //Getting data from the server
 var content = document.getElementsByClassName("content")[0];
 async function getDetailBySection(section) {
